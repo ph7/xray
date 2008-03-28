254867bc0e2ea3de042379ff2c4fdb41f28636f8
ph7 <ph7@5dde6e04-f255-48cb-b2b1-8039b2f1cec5>
2008-03-28 07:24:27 +0000

    Fully functional Rails tracing
    git-svn-id: svn+ssh://rubyforge.org/var/svn/xray@16 5dde6e04-f255-48cb-b2b1-8039b2f1cec5

--- lib/xray/dtrace/rails/enable_tracing.rb
+++ lib/xray/dtrace/rails/enable_tracing.rb
@@ -1,3 +1,18 @@
+# Require this file to enable out the box DTrace tracing 
+# for your Rails application
+#
+# You typically change your environment.rb to require it after config
+# initialization:
+#
+#   Rails::Initializer.run do |config|
+#
+#     ...
+#
+#     config.after_initialize do
+#       require "xray/dtrace/rails/enable_tracing"
+#     end  
+#   end
+
 require 'xray/dtrace/tracer'
 if Object.const_defined? :ActionController
   puts "Enabling controller tracing"
@@ -7,9 +22,4 @@ end
 if Object.const_defined? :ActiveRecord
   puts "Enabling DB tracing"
   require "xray/dtrace/rails/active_record_connection_tracing_extension"
-end
-
-if Object.const_defined? :ActionView
-  puts "Enabling rendering tracing"
-  require "xray/dtrace/rails/action_view_tracing_extension"
 end
