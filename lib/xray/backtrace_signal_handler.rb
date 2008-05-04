4a78150b694aefd4c28fbffed1c43ba414a085a7
Philippe Hanrigou <philippe.hanrigou@gmail.com>
2008-05-04 16:07:06 -0700

    Nicer thread dump formatter

--- lib/xray/backtrace_signal_handler.rb
+++ lib/xray/backtrace_signal_handler.rb
@@ -4,19 +4,21 @@
 # Trigger it with: kill -QUIT <pid>
 #  
 trap "QUIT" do
-  STDERR.puts "=============== XRay - Thread Dump ==============="
   if Kernel.respond_to? :caller_for_all_threads
+    STDERR.puts "\n=============== XRay - Thread Dump ==============="
     caller_for_all_threads.each_pair do |thread, stack|
       thread_description = thread.inspect
       thread_description << " [main]" if thread == Thread.main
       thread_description << " [current]" if thread == Thread.current
       thread_description << " alive=#{thread.alive?}"
       thread_description << " priority=#{thread.priority}"
-      STDERR.puts "\n#{thread_description}\n    #{stack.join("\n      \\_ ")}\n"
+      thread_separator = "-" * 78
+      STDERR.puts "\n#{thread_separator}\n#{thread_description}\n#{thread_separator}\n    #{stack.join("\n      \\_ ")}\n"
     end
   else
+    STDERR.puts "=============== XRay - Current Thread Backtrace ==============="
     STDERR.puts "Current thread : #{Thread.inspect}"
     STDERR.puts caller.join("\n    \\_ ")
   end
-  STDERR.puts "=============== XRay - Done ==============="
+  STDERR.puts "\n=============== XRay - Done ===============\n"
 end
